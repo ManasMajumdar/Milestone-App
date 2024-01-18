156d5f6bf79d248d4d5265af4c39acb91566a6a6
unknown <manas.majumdar@minditsystems.com>
2024-01-18 22:56:12 +0530

    Footer change

--- milestonemanagementweb/src/component/Footer.js
+++ milestonemanagementweb/src/component/Footer.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 function Footer() {
   return (
-<div className="footer">
-  <div className="container py-0">
-    <hr className="bg-color-light opacity-1 mb-0" />
-    <div className="row justify-content-center py-3">
-      <div className="col-auto">
-        <p className="text-color-light opacity-5 text-3 mb-0">
-          Mind IT <sup>®</sup> © Copyright 2024. All Rights Reserved.
-        </p>
+    <div className="footer">
+      <div className="container py-0">
+        <hr className="bg-color-light opacity-1 mb-0" />
+        <div className="row justify-content-center py-3">
+          <div className="col-auto">
+            <p className="text-color-light opacity-5 text-3 mb-0">
+              Manas <sup>®</sup> © Copyright 2024. All Rights Reserved.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
   );
 }
 export default Footer;
